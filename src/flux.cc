7fb2790bf9dd693937198d5f6db827ef0932e38f
Aditya Diwakar <aditya@diwakar.io>
2022-12-14 13:46:41 -0500

    add cpr library with example

--- src/flux.cc
+++ src/flux.cc
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main() {
-  std::cout << "Hello, world." << std::endl;
+  std::cout << "Starting request..." << std::endl;
+  
+  cpr::Response r = cpr::Get(cpr::Url{"https://api.diwakar.io/"});
+
+  std::cout << "Status code: " << r.status_code << '\n';
+  std::cout << "Header:\n";
+
+  for (const std::pair<const std::basic_string<char>, std::basic_string<char>>& kv : r.header) {
+    std::cout << '\t' << kv.first << ':' << kv.second << '\n';
+  }
+  std::cout << "\n" << "Text: " << r.text << '\n';
+
+  return 0;
 }
