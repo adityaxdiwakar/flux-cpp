64d44700fdd9fb3b160812d5698ea7477b86fae4
Aditya Diwakar <aditya@diwakar.io>
2022-12-16 20:07:15 -0500

    add comments

--- include/session.hpp
+++ include/session.hpp
@@ -1,6 +1,12 @@
 #include <string>
 #include <nlohmann/json.hpp>
+#include "cpr/cpr.h"
 #pragma once
+
+enum request_type {
+  GET,
+  POST
+};
 
 class AmeritradeSession {
   private:
@@ -8,11 +14,12 @@ class AmeritradeSession {
     std::string consumer_key;
     std::string root_url;
     std::string access_token;
+    void init_access_token();
 
   public:
     AmeritradeSession(std::string refresh, std::string consumer_key, std::string root_url);
     AmeritradeSession();
     friend std::ostream& operator<<(std::ostream &os, const  AmeritradeSession& s);
     friend void to_json(nlohmann::json& j, const AmeritradeSession& s);
-    void get_access_token();
+    std::string get_access_token();
 };
--- src/session.cc
+++ src/session.cc
@@ -10,13 +10,29 @@
 
 using namespace std;
 
+/**
+ * Construct an AmeritradeSession with neccesary information to initialize.
+ *
+ * @param a refresh token to generate access tokens
+ * @param a consumer_key generated from the TDAmeritrade Developer portal
+ * @param a root_url to make an API request (useful for /v1/ or /v2/ in future)
+ */
 AmeritradeSession::AmeritradeSession(string refresh, string consumer_key, string root_url) 
   : refresh(refresh),
     consumer_key(consumer_key),
-    root_url(root_url) {}
+    root_url(root_url) {
+
+  init_access_token();
+}
 
 AmeritradeSession::AmeritradeSession() {}
 
+/**
+ * Set a JSON object with the representation of an AmeritradeSession.
+ *
+ * @param a JSON object to be written into
+ * @param a constant (immutable) AmeritradeSession reference to JSON represent
+ */
 void to_json(nlohmann::json& j, const AmeritradeSession& s) {
   j = nlohmann::json {
     {"refresh", s.refresh},
@@ -25,13 +41,24 @@ void to_json(nlohmann::json& j, const AmeritradeSession& s) {
   };
 }
 
+/**
+ * Overload stream operator to print out an AmeritradeSession.
+ *
+ * @return a stream object with AmeritradeSession added
+ */
 ostream& operator<<(ostream &os, const AmeritradeSession& s) {
   cout << (nlohmann::json) s;
-
   return os;
 }
 
-void AmeritradeSession::get_access_token() {
+/**
+ * Dynamically initialize access token.
+ *
+ * This function initializes the access token and stores it as a private 
+ * session field. TODO: This function only initializes or regenerates
+ * a token if the token has expired.
+ */
+void AmeritradeSession::init_access_token() {
   auto req_payload = cpr::Payload{
     {"grant_type", gt_refresh_token},
     {"refresh_token", refresh},
@@ -45,4 +72,19 @@ void AmeritradeSession::get_access_token() {
   auto access_rsp = j.get<oauth_rsp>();
 
   this->access_token = access_rsp.access_token;
+}
+
+/**
+ * Return a valid access token.
+ *
+ * This function will use an existing session object and return a valid access
+ * token or regenerate one if the existing token has expired. This function
+ * only needs to be called if a token is needed for external purposes, such
+ * as connecting with fluxcpp-socket.
+ *
+ * @return a valid TDAmeritrade API access token
+ */
+string AmeritradeSession::get_access_token() {
+  init_access_token();
+  return access_token;
 }
