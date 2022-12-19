8b2ce2d4e60961e891f13f43b83f1160011bae14
Aditya Diwakar <aditya@diwakar.io>
2022-12-18 19:04:07 -0500

    define json handlers for custom market hour types

--- include/markethours.hpp
+++ include/markethours.hpp
@@ -2,15 +2,35 @@
 #include <vector>
 #include <unordered_map>
 
+#include "nlohmann/json.hpp"
+
+enum class market_type {
+  MARKET_EQUITY,
+  MARKET_OPTION,
+  MARKET_FUTURE,
+  MARKET_BOND,
+  MARKET_FOREX,
+};
+
+
+const char* get_str(market_type t);
+const market_type get_market_type(std::string s);
+
 struct market_times {
   std::string start;
   std::string end;
+
+  friend void from_json(const nlohmann::json&, market_times&);
+  friend void to_json(nlohmann::json&, const market_times&);
 };
 
 struct session_hours {
-  std::vector<market_times> pre_market;
-  std::vector<market_times> regular_market;
-  std::vector<market_times> post_market;
+  std::optional<std::vector<market_times>> pre_market;
+  std::optional<std::vector<market_times>> regular_market;
+  std::optional<std::vector<market_times>> post_market;
+
+  friend void from_json(const nlohmann::json&, session_hours&);
+  friend void to_json(nlohmann::json&, const session_hours&);
 };
 
 struct market_hours {
@@ -22,6 +42,16 @@ struct market_hours {
   std::string product_name;
   bool is_open;
   session_hours session_hours;
+
+  friend void from_json(const nlohmann::json&, market_hours&);
+  friend void to_json(nlohmann::json&, const market_hours&);
 };
 
-using hours_rsp_type = std::unordered_map<std::string, std::unordered_map<std::string, market_hours>>;
+using singled_hours_rsp_t = std::unordered_map<std::string, market_hours>;
+void from_json(const nlohmann::json& j, singled_hours_rsp_t&);
+void to_json(nlohmann::json& j, const singled_hours_rsp_t&);
+
+using hours_rsp_type = std::unordered_map<market_type, singled_hours_rsp_t>;
+void from_json(const nlohmann::json&, hours_rsp_type&);
+void to_json(nlohmann::json&, const hours_rsp_type&);
+
---
+++ src/markethours.cc
@@ -0,0 +1,117 @@
+#include "markethours.hpp"
+#include "nlohmann/json.hpp"
+#include <vector>
+
+using namespace std;
+
+void to_json(nlohmann::json& j, const market_times& sh) {
+  j = nlohmann::json{
+    {"start", sh.start},
+    {"end", sh.end}
+  };
+}
+
+void from_json(const nlohmann::json& j, market_times& sh) {
+  j.at("start").get_to(sh.start);
+  j.at("end").get_to(sh.end);
+}
+
+void from_json(const nlohmann::json& j, session_hours& sh) {
+  if (j.count("preMarket"))
+    sh.pre_market = j.at("preMarket").get<vector<market_times>>();
+
+  if (j.count("regularMarket"))
+    sh.pre_market = j.at("regularMarket").get<vector<market_times>>();
+
+  if (j.count("postMarket"))
+    sh.pre_market = j.at("postMarket").get<vector<market_times>>();
+}
+
+void to_json(nlohmann::json& j, const session_hours& sh) {
+  if (sh.pre_market != nullopt) 
+    j["pre_market"] = sh.pre_market.value();
+  if (sh.regular_market != nullopt) 
+    j["regular_market"] = sh.regular_market.value();
+  if (sh.post_market != nullopt) 
+    j["post_market"] = sh.post_market.value();
+}
+
+void from_json(const nlohmann::json& j, market_hours& mh) {
+  j.at("date").get_to(mh.date);
+  j.at("marketType").get_to(mh.market_type);
+  j.at("exchange").get_to(mh.exchange);
+  j.at("category").get_to(mh.category);
+  j.at("product").get_to(mh.product);
+  j.at("productName").get_to(mh.product_name);
+  j.at("isOpen").get_to(mh.is_open);
+  j.at("sessionHours").get_to(mh.session_hours);
+}
+
+void to_json(nlohmann::json& j, const market_hours& mh) {
+  j = nlohmann::json{
+    {"date", mh.date},
+    {"market_type", mh.market_type},
+    {"exchange", mh.exchange},
+    {"category", mh.category},
+    {"product", mh.product},
+    {"product_name", mh.product_name},
+    {"is_open", mh.is_open},
+    {"session_hours", mh.session_hours},
+  };
+}
+
+void from_json(const nlohmann::json& j, singled_hours_rsp_t& s) {
+  for (auto& entry: j.items())
+    s[entry.key()] = entry.value().get<market_hours>();
+}
+
+void to_json(nlohmann::json& j, const singled_hours_rsp_t& s) {
+  for (const pair<string, market_hours>& si : s)
+    j[si.first] = si.second;
+}
+
+void from_json(const nlohmann::json& j, hours_rsp_type& s) {
+  for (auto& entry: j.items()) {
+    market_type label;
+    if (entry.key() == "equity")      label = market_type::MARKET_EQUITY;
+    else if (entry.key() == "future") label = market_type::MARKET_FUTURE;
+    else if (entry.key() == "option") label = market_type::MARKET_OPTION;
+    else if (entry.key() == "bond")   label = market_type::MARKET_BOND;
+    else if (entry.key() == "forex")  label = market_type::MARKET_FOREX;
+
+    s[label] = entry.value().get<singled_hours_rsp_t>();
+  }
+}
+
+void to_json(nlohmann::json& j, const hours_rsp_type& s) {
+  for (const pair<market_type, singled_hours_rsp_t>& si : s) {
+    const char* label;
+    switch (si.first) {
+      case market_type::MARKET_EQUITY:  label = "equity";   break;
+      case market_type::MARKET_FUTURE:  label = "future";   break;
+      case market_type::MARKET_FOREX:   label = "forex";    break;
+      case market_type::MARKET_OPTION:  label = "option";   break;
+      case market_type::MARKET_BOND:    label = "bond";     break;
+    }
+
+    j[label] = si.second;
+  }
+}
+
+const char* get_str(market_type t) {
+  switch (t) {
+    case market_type::MARKET_EQUITY:   return "equity";
+    case market_type::MARKET_FUTURE:   return "future";
+    case market_type::MARKET_OPTION:   return "option";
+    case market_type::MARKET_BOND:     return "bond";
+    case market_type::MARKET_FOREX:    return "forex";
+  }
+}
+
+const market_type get_market_type(std::string s) {
+  if (s == "equity")        return market_type::MARKET_EQUITY;
+  else if (s == "bond")     return market_type::MARKET_BOND;
+  else if (s == "forex")    return market_type::MARKET_FOREX;
+  else if (s == "option")   return market_type::MARKET_OPTION;
+  else                      return market_type::MARKET_FUTURE;
+}
