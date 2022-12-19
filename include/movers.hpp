c38877cce707ff62cdb40f3a4fbd2ab67c21fdf3
Aditya Diwakar <aditya@diwakar.io>
2022-12-19 14:43:24 -0500

    add movers functionality

---
+++ include/movers.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+#include "nlohmann/json.hpp"
+
+enum class index          { COMPX, DJI, SPX };
+enum class change_metric  { RAW, PERCENT };
+enum class direction      { UP, DOWN };
+
+struct mover {
+  std::string description;
+  std::string symbol;
+
+  double change;
+  double last;
+  int64_t total_volume;
+
+  direction direction;
+
+  friend void from_json(const nlohmann::json&, mover&);
+  friend void to_json(nlohmann::json&, const mover&);
+};
