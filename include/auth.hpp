923d660b555ecf9ce8a86d84076b17dc24f5b8d9
Aditya Diwakar <aditya@diwakar.io>
2022-12-30 19:09:43 -0500

    move auth into namespace

--- include/auth.hpp
+++ include/auth.hpp
@@ -2,13 +2,17 @@
 #include "nlohmann/json.hpp"
 #pragma once
 
-const std::string gt_refresh_token = "refresh_token";
+namespace flux {
+  namespace auth {
+    const std::string gt_refresh_token = "refresh_token";
 
-struct oauth_rsp {
-  std::string access_token;
-  std::string scope;
-  std::string token_type;
-  int expires_in;
+    struct oauth_rsp {
+      std::string access_token;
+      std::string scope;
+      std::string token_type;
+      int expires_in;
 
-  NLOHMANN_DEFINE_TYPE_INTRUSIVE(oauth_rsp, access_token, scope, token_type, expires_in);
+      NLOHMANN_DEFINE_TYPE_INTRUSIVE(oauth_rsp, access_token, scope, token_type, expires_in);
+    };
+  };
 };
